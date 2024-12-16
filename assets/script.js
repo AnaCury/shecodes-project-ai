9bf21e980c0bc0e2f5a019f0f0ae1cc225f04f71
Ana Cury <75744584+AnaCury@users.noreply.github.com>
2024-12-16 15:35:48 -0400

    adding ai

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "la la la la la",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "59bb440b331f3618c8f63oceca0t0e45";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let prompt = `User instructions Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to write a 4 lines poem. Make sure to follow the user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let form = document.querySelector("#form");
